abe5bb96c9a7c96c8d7b8017840ea3791e55788e
Cy Bunayog <cybunayog@gmail.com>
2020-04-17 14:38:09 -0700

    Fixed React Navigation dependencies AGAIN

--- App.js
+++ App.js
@@ -1,3 +1,22 @@
-import Quiz from './screens/Quiz';
+import 'react-native-gesture-handler';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
-export default Quiz;
+import Quiz from './screens/Quiz';
+import QuizIndex from './screens/QuizIndex';
+
+const MainStack = createStackNavigator({
+  QuizIndex: {
+    screen: QuizIndex,
+    navigationOptions: {
+      headerTitle: 'Quizzes',
+    },
+  },
+  Quiz: {
+    screen: Quiz,
+  },
+});
+
+const AppNavigator = createAppContainer(MainStack);
+
+export default AppNavigator;
